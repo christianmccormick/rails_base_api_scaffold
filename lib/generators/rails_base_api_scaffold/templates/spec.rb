2482aff7cc115731bd898597123d39597dd6e954
Christian McCormick <cmccormick421@gmail.com>
2017-12-28 10:11:03 -0800

    Add delete spec

--- lib/generators/rails_base_api_scaffold/templates/spec.rb
+++ lib/generators/rails_base_api_scaffold/templates/spec.rb
@@ -34,7 +34,7 @@ RSpec.describe '<%= controller_class_name %>Controller', :type => :request do
     context 'user is not created' do
       it 'returns a 401 if user is not authenticated' do
         posted_data = {
-            <%= mock_editable_attribute_key %>: <%= mock_editable_attribute_value %>
+          <%= mock_editable_attribute_key %>: <%= mock_editable_attribute_value %>
         }
         response = oauth_post api_v1_<%= plural_name %>_path, { <%= singular_name %>: posted_data }
         expect(response.status).to eq 401
@@ -42,12 +42,12 @@ RSpec.describe '<%= controller_class_name %>Controller', :type => :request do
     end
 
     context 'user created' do
-      it 'creates a <%= singular_name %>  for authenticated user' do
+      it 'creates a <%= singular_name %> for authenticated user' do
         user = create(:admin)
         prepare_user_oauth user
 
         posted_data = {
-            <%= mock_editable_attribute_key %>: <%= mock_editable_attribute_value %>
+          <%= mock_editable_attribute_key %>: <%= mock_editable_attribute_value %>
         }
 
         response = oauth_post api_v1_<%= plural_name %>_path, { <%= singular_name %>: posted_data }
@@ -78,7 +78,7 @@ RSpec.describe '<%= controller_class_name %>Controller', :type => :request do
         prepare_user_oauth user
 
         posted_data =  {
-            <%= mock_editable_attribute_key %>: <%= mock_editable_attribute_value %>
+          <%= mock_editable_attribute_key %>: <%= mock_editable_attribute_value %>
         }
 
         response = oauth_put api_v1_<%= singular_name %>_path(<%= singular_name %>), { <%= singular_name %>: posted_data }
@@ -88,6 +88,32 @@ RSpec.describe '<%= controller_class_name %>Controller', :type => :request do
 
         expect(json_response['id']).to eq <%= singular_name %>.id
         expect(json_response['<%= mock_editable_attribute_key %>']).to eq <%= mock_editable_attribute_value %>
+      end
+    end
+  end
+
+  describe 'DELETE /<%= plural_name %>/:id' do
+    context 'user is not created' do
+      it 'returns a 401 if user is not authenticated' do
+        <%= singular_name %> = create(:<%= singular_name %>)
+        response = oauth_del api_v1_<%= singular_name %>_path(<%= singular_name %>)
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'user created' do
+      it 'updates a <%= singular_name %> for authenticated user' do
+        user = create(:admin)
+        <%= singular_name %> = create(:<%= singular_name %>)
+
+        prepare_user_oauth user
+
+        response = oauth_del api_v1_<%= singular_name %>_path(<%= singular_name %>)
+        expect(response.status).to eq 200
+
+        json_response = JSON.parse response.body
+
+        expect(json_response['message']).to eq '<%= human_name %> deleted'
       end
     end
   end
